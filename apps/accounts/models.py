32486bfbfc1b3eb62a5223f59ba25885faef0b21
Arya <aryasandbhor03@gmail.com>
2026-09-01 21:24:19 +0530

    add apps folder and custom user model with roles

---
+++ apps/accounts/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+class User(AbstractUser):
+    class Role(models.TextChoices):
+        ADMIN="ADMIN", "Admin"
+        DOCTOR="DOCTOR", "Doctor"
+        RECEPTIONIST="RECEPTIONIST","Receptionist"
+        PATIENT="PATIENT", "Patient"
+    role=models.CharField(max_length=20, choices=Role.choices, default=Role.PATIENT)
+    def __str__(self):
+        return f"{self.username} ({self.role})"
+    
+# Create your models here.
--- config/settings/base.py
+++ config/settings/base.py
@@ -9,13 +9,13 @@ https://docs.djangoproject.com/en/6.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.1/ref/settings/
 """
-
+import sys
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
-
+sys.path.insert(0,str(BASE_DIR/'apps'))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/6.1/howto/deployment/checklist/
 
@@ -37,7 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'accounts',
 ]
+
+AUTH_USER_MODEL="accounts.User"
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
